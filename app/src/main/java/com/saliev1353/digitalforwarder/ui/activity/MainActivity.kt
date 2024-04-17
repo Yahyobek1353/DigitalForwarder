6bb99e8836c8f7682c58ea7490ffb6a51b66f330
Geeks <salievyt@gmail.com>
2024-04-17 15:09:12 +0600

    V2

---
+++ app/src/main/java/com/saliev1353/digitalforwarder/ui/activity/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.saliev1353.digitalforwarder.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.saliev1353.digitalforwarder.R
+import com.saliev1353.digitalforwarder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val navController: NavController by lazy {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navHostFragment.navController
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        initBottomNav()
+    }
+    private fun initBottomNav() {
+        binding.bottomNav.apply {
+            setupWithNavController(navController)
+        }
+    }
+
+}
