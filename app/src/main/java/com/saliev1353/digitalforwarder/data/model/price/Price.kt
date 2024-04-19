dc5d88082d302ea76f385636716af78d48de7d10
muza27 <gadievafarangis2@gmail.com>
2024-04-19 19:19:27 +0600

    v6.1

---
+++ app/src/main/java/com/saliev1353/digitalforwarder/data/model/price/Price.kt
@@ -0,0 +1,9 @@
+package com.saliev1353.digitalforwarder.data.model.price
+
+import java.io.Serializable
+
+data class Price(
+    val income :String,
+    val remove : String,
+    val allPrice : String
+):Serializable
